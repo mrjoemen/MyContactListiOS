e9066aa5735013aab2fe0fd50e38589e8201c4ac
Arlin Cabral <arlincabral@Arlins-MacBook-Air.local>
2021-04-07 16:30:09 -0400

    first commit

---
+++ MyContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyContactList
+//
+//  Created by Arlin Cabral on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
